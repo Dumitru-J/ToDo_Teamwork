018f4f802e4ad0887eeddbf222ad71fd2c725ded
Dumitru Jelezneac <dumitru.jelezneac@htlpinkfafeld.at>
2025-09-19 20:30:15 +0200

    frontend v1

---
+++ Frontend/app.js
@@ -0,0 +1,38 @@
+import { getToDos, registrieren } from "./model";
+
+let registrierButton = document.querySelector('#registrieren');
+
+registrierButton.addEventListener('click', function() {
+    let benutzername = document.querySelector('#benutzername').value;
+    let passwort = document.querySelector('#passwort').value;
+    registrieren(benutzername, passwort).then(response => {
+        if (response.success) {
+            alert('Registrierung erfolgreich!');
+        } else {
+            alert('Fehler bei der Registrierung: ' + response.error);
+        }
+    }).catch (err => {
+        alert('Server nicht erreichbar: ' + err.message);
+    });
+});
+
+let todo_list;
+
+let loginButton = document.querySelector('#anmelden');
+loginButton.addEventListener('click', function() {
+    let benutzername = document.querySelector('#benutzername').value;
+    let passwort = document.querySelector('#passwort').value;
+    anmelden(benutzername, passwort).then(response => {
+        if (response.success) {
+            alert('Anmeldung erfolgreich!');
+            window.location.href = 'liste.html';
+            todo_list = getToDos(benutzername);
+        } else {
+            alert('Fehler bei der Anmeldung: ' + response.error);
+        }
+    }).catch (err => {
+        alert('Server nicht erreichbar: ' + err.message);
+    });
+});
+
+
